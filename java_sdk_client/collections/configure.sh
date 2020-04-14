43ed468e74ce22acd4f75002537299a91820cb82
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2020-04-14 05:50:40 -0700

    config params -1

--- java_sdk_client/collections/configure.sh
+++ java_sdk_client/collections/configure.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar /tmp/javaclient.jar -i $CLUSTER -b $BUCKET -s $SCOPE -c $COLLECTION -n $N -pc $PC -pu $PU -pd $PD -pr $PR -l $L -dsn $DSN -dpx $DPX -dsx $DSX -dt $DT -o $O
+java -jar /tmp/javaclient.jar -i $CLUSTER -b $BUCKET -s $SCOPE -c $COLLECTION -n $N 
+#-pc $PC -pu $PU -pd $PD -pr $PR -l $L -dsn $DSN -dpx $DPX -dsx $DSX -dt $DT -o $O
